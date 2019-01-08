9eb1e3a85c0b66f01cdcd4896cb8aeea5f1c7c9b
Thomas Dornell <thomasdornell@gmail.com>
2019-01-08 12:12:48 +0100

    Updates

--- precache-manifest.55033f8ee24c303942db48ee06e85801.js
+++ precache-manifest.55033f8ee24c303942db48ee06e85801.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-calculator/static/js/runtime~main.a2ecf2e6.js"
   },
   {
-    "revision": "fca8975e656466199248",
-    "url": "/react-calculator/static/js/main.fca8975e.chunk.js"
+    "revision": "e277cc55a22b1b378370",
+    "url": "/react-calculator/static/js/main.e277cc55.chunk.js"
   },
   {
     "revision": "975081c91dfa23810deb",
     "url": "/react-calculator/static/js/1.975081c9.chunk.js"
   },
   {
-    "revision": "fca8975e656466199248",
-    "url": "/react-calculator/static/css/main.d97a4528.chunk.css"
+    "revision": "e277cc55a22b1b378370",
+    "url": "/react-calculator/static/css/main.d6d6e250.chunk.css"
   },
   {
-    "revision": "ac24a8f4cbac99847eca1027281f25f0",
+    "revision": "02e5719ce8861d31907469bf074650df",
     "url": "/react-calculator/index.html"
   }
 ];
